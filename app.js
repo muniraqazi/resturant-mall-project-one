5ab66bab947b586ea4b6a17ed710b74777483a8b
Munira <muniraqazi98@gmail.com>
2021-07-07 21:39:49 -0400

    added some comments

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
-// console.log ("hello");
-
-
+//open and close (hide) the modal on click event
 const toggleModal = () => {
   document.querySelector('.modal').classList.toggle('modalHidden');
-  // console.log("clicked");
 };
 
 document.querySelector('#openModal').addEventListener('click', toggleModal);
 
+//submit form on click 
 document.querySelector('#submitModal').addEventListener('submit', (event) => {
   event.preventDefault();
   toggleModal();
 });
 
+//close modal on click
 document.querySelector('.modalCloseBtn').addEventListener('click', toggleModal);
 
+//tutorial used for creating the modal
 //https://www.youtube.com/watch?v=KX0NR5HtQ1Q&ab_channel=JuniorDeveloperCentral
